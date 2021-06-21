4d3fc5b8f2171d000b8e271723b5932f0a5d95da
mvoevodskiy <mv@aaa0.ru>
2021-06-21 08:42:49 +0300

    Added payment methods to default values

--- src/defaultvalues.js
+++ src/defaultvalues.js
@@ -5,8 +5,54 @@ module.exports = [
     cost: 0,
     active: false,
     controller: 'mvlShopYookassa',
+    rank: 2,
     extended: {
       type: 'bank_card'
+    }
+  },
+  {
+    name: 'ЮКасса Банковская карта (сохранить)',
+    key: 'yookassaBankCardSave',
+    cost: 0,
+    active: false,
+    controller: 'mvlShopYookassa',
+    rank: 3,
+    extended: {
+      type: 'bank_card',
+      save_payment_method: true
+    }
+  },
+  {
+    name: 'ЮКасса ЮМани',
+    key: 'yookassaYoomoney',
+    cost: 0,
+    active: false,
+    controller: 'mvlShopYookassa',
+    rank: 4,
+    extended: {
+      type: 'yoo_money'
+    }
+  },
+  {
+    name: 'ЮКасса Киви-кошелёк',
+    key: 'yookassaQiwi',
+    cost: 0,
+    active: false,
+    controller: 'mvlShopYookassa',
+    rank: 5,
+    extended: {
+      type: 'qiwi'
+    }
+  },
+  {
+    name: 'ЮКасса Webmoney',
+    key: 'yookassaWebmoney',
+    cost: 0,
+    active: false,
+    controller: 'mvlShopYookassa',
+    rank: 6,
+    extended: {
+      type: 'webmoney'
     }
   }
 ]
